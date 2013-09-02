b9cb92b1a1b0cb40e0fa30a6f74e1b8621888218
Juan Reche <jareche@gmail.com>
2013-09-02 15:49:43 +0000

    modify web.js

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var file = fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+  });
+  response.send($file);
 });
 
 var port = process.env.PORT || 5000;
